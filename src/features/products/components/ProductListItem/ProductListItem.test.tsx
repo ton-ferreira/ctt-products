32d20de852e780c34231b5f09e34f68d34f24b27
Ewerton Ferreira <ewertonferreira.dev@icloud.com>
2025-06-01 19:16:00 +0100

    test(ProductListItem): add basic tests

---
+++ src/features/products/components/ProductListItem/ProductListItem.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { CategoryId, CategoryLabels } from "../../stores/types/categories";
+
+const mockedProduct = {
+  id: "c112bd93-7792-4afa-8bea-aa1b6ccdfb75",
+  stock: 1,
+  description: "Some nice product",
+  categories: [CategoryId.Food],
+  price: 10.0,
+};
+
+describe("ProductListItem", () => {
+  it("should show the products info received by its props", () => {
+    render(<ProductListItem product={mockedProduct} />);
+    expect(screen.getByText(mockedProduct.stock)).toBeInTheDocument();
+    expect(screen.getByText(mockedProduct.description)).toBeInTheDocument();
+    expect(
+      screen.getByText(CategoryLabels[CategoryId.Food])
+    ).toBeInTheDocument();
+    expect(screen.getByText(mockedProduct.price)).toBeInTheDocument();
+  });
+});
