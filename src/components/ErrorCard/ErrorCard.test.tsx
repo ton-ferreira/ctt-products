32555337c35eacecd2e9b8f92dfd903c9e06f8d8
Ewerton Ferreira <ewertonferreira.dev@icloud.com>
2025-06-01 18:51:17 +0100

    feat(ErrorCard): create ErrorCard component

--- src/components/ErrorCard/ErrorCard.test.tsx
+++ src/components/ErrorCard/ErrorCard.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import ErrorCard from "./ErrorCard";
 
 describe("ErrorCard", () => {
   it("should show a text when it renders", () => {
-    const error = "[404] Error message is missing!!!";
-    render(<ErrorCard error={error} />);
-    expect(screen.getByText(error));
+    const errorMessage = "[404] Error message is missing!!!";
+    render(<ErrorCard errorMessage={errorMessage} />);
+    expect(screen.getByText(errorMessage));
   });
 });
