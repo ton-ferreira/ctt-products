39d969fbf7f040f18e5d6e2a295b5a888bb28116
Ewerton Ferreira <ewertonferreira.dev@icloud.com>
2025-06-01 18:44:15 +0100

    test(EmptyCard): add EmptyCard tests

---
+++ src/components/EmptyCard/EmptyCard.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+describe("EmptyCard", () => {
+  it("should render title and description", () => {
+    const title = "No items found.";
+    const description = "Something descriptive...";
+    render(<EmptyCard title={title} description={description} />);
+
+    expect(screen.getByRole("heading", { name: title })).toBeInTheDocument();
+    expect(screen.getByText(description)).toBeInTheDocument();
+  });
+});
