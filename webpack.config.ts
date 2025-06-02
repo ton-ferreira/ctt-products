d513bc38fa501c087bf3846d591a51bcad0de830
Ewerton Ferreira <ewertonferreira.dev@icloud.com>
2025-06-02 16:29:37 +0100

    chore(webpack): setup file loaders in webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -25,6 +25,25 @@ const config: Configuration = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
