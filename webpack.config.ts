5a9c5ac5d9c2b7706d863e025b85b3208dee6393
Ewerton Ferreira <ewertonferreira.dev@icloud.com>
2025-06-02 13:04:31 +0100

    chore(webpack): setup sass loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -21,6 +21,10 @@ const config: Configuration = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   resolve: {
